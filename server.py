209930e13d16985d9e6c676a23a248d4aea4b320
Jonathan Kalmar <jkalmar12@gmail.com>
2021-03-19 11:16:38 -0700

    import old snake

--- server.py
+++ server.py
@@ -8,6 +8,37 @@ This is a simple Battlesnake server written in Python.
 For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
 
+def open_spaces(data):
+    spaces = []
+    for y in range(0,data['board']['height']):
+        for x in range(0,data['board']['width']):
+            spaces.append({"y": y,"x": x})
+    for snake in data['board']['snakes']:
+        for position in snake['body']:
+            if position in spaces: spaces.remove(position)
+    return spaces
+
+def next_pos(head, dir):
+    #returns the next position of my snake if moved in a given direction
+    head=head.copy()
+    if dir == "up":
+        head['y']-=1
+    elif dir == "down":
+        head['y']+=1
+    elif dir == "left":
+        head['x']-=1
+    elif dir == "right":
+        head['x']+=1
+    return head
+    
+def possible_movez(head):
+    data = bottle.request.json
+    directions=["left", "right", "up", "down"]
+    possible_moves = []
+    for dir in directions:
+        if next_pos(head, dir) in open_spaces(data):
+            possible_moves.append(dir)
+    return possible_moves
 
 class Battlesnake(object):
     @cherrypy.expose
@@ -19,9 +50,9 @@ class Battlesnake(object):
         return {
             "apiversion": "1",
             "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "color": "#2E66FF",  # TODO: Personalize
+            "head": "bwc-ski",  # TODO: Personalize
+            "tail": "bolt",  # TODO: Personalize
         }
 
     @cherrypy.expose
@@ -30,7 +61,6 @@ class Battlesnake(object):
         # This function is called everytime your snake is entered into a game.
         # cherrypy.request.json contains information about the game that's about to be played.
         data = cherrypy.request.json
-
         print("START")
         return "ok"
 
@@ -43,10 +73,57 @@ class Battlesnake(object):
         # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+    move = "none"
+    # Choose a random direction to move in
+    head = data['you']['body'][0]
+    possible_moves = possible_movez(head)
+    directions=["left", "right", "up", "down"]
+    for move in possible_moves:
+        if possible_movez(next_pos(head,move)) ==[]:
+            possible_moves.remove(move)
+    #move = random.choice(possible_moves)
+    dict={}
+    food = "false"
+    for mv in possible_moves:
+        if next_pos(head, mv) in data['board']['food']:
+            move = mv
+            food = "true"
+    
+    if food =="false":
+        for mv in possible_moves:
+            i=0
+            if mv =="up":
+                head1=head.copy()
+                head1['y']-=1
+                while next_pos(head1,mv) in open_spaces(data):
+                    head1['y']-=1
+                    i+=1
+            elif mv =="down":
+                head2=head.copy()
+                head2['y']+=1
+                while next_pos(head2,mv) in open_spaces(data):
+                    head2['y']+=1
+                    i+=1
+            elif mv =="left":
+                head3=head.copy()
+                head3['x']-=1
+                while next_pos(head3,mv) in open_spaces(data):
+                    head3['x']-=1
+                    i+=1
+            elif mv =="right":
+                head4=head.copy()
+                head4['x']+=1
+                while next_pos(head4,mv) in open_spaces(data):
+                    head4['x']+=1
+                    i+=1
+            dict[mv]=i
+    move = max(dict, key=lambda key: dict[key])
 
+    #repeat for other directions
+    #out of the remaining directions, choose the one that gives the most distance from a wall or from another snake.
+    #if one space away from food, get the food
+    
+    
         print(f"MOVE: {move}")
         return {"move": move}
 
